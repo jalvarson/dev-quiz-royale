aa2ed64ea4bdd2640bad0f5d79bdf6749b04488c
Jonas Alvarson <jonas.alvarsson@gmail.com>
2025-01-09 20:48:34 +0100

    configure Google Fonts module to include Inter and Passion One families

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,10 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: ['@nuxtjs/google-fonts', '@nuxt/image'],
+    googleFonts: {
+        families: {
+        Inter: true,
+        "Passion One": true,
+        },
+    },
 })
