ddbc5d27cb4df2bc478f25f91a6eb1a29adfe7bf
Jonas Alvarson <jonas.alvarsson@gmail.com>
2025-01-09 22:29:33 +0100

    add SCSS configuration and include main stylesheet in Nuxt.js setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,10 +3,23 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: ['@nuxtjs/google-fonts', '@nuxt/image'],
-    googleFonts: {
-        families: {
-        Inter: true,
-        "Passion One": true,
-        },
+  googleFonts: {
+    families: {
+      Inter: true,
+      'Passion One': true,
     },
-})
+  },
+  css: ['~/assets/styles/main.scss'],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `
+          @use "~/assets/styles/variables/colors.scss" as *;
+          @use "~/assets/styles/mixins/typography.scss" as *;
+        `,
+        },
+      },
+    },
+  },
+});
