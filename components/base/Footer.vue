d29f9951335de569419cd3422d7465ca1ad63edb
Jonas Alvarson <jonas.alvarsson@gmail.com>
2025-01-11 21:34:02 +0100

    Add footer component

---
+++ components/base/Footer.vue
@@ -0,0 +1,24 @@
+<script setup>
+</script>
+
+<template>
+    <div class="footer">
+        <span>Made with ❤️ by <a href="https://github.com/jalvarson/" target="_blank">Jonas Alvarson</a></span>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.footer {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 2rem 4rem;
+    text-align: center;
+    color: $on-surface;
+    @include bodySmall;
+
+    a {
+        font-weight: 700;
+    }
+}
+</style>
