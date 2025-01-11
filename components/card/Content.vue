d307580ee12369baf73559fe2e0440da1a66c83a
Jonas Alvarson <jonas.alvarsson@gmail.com>
2025-01-11 14:27:39 +0100

    Add Card component

---
+++ components/card/Content.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="card-content">
+    <h3 class="card-content__title">{{ title }}</h3>
+    <p class="card-content__body">{{ body }}</p>
+    <NuxtImg
+      v-if="image"
+      :src="image"
+      :alt="imageAlt"
+      class="card-content__image"
+    />
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  title: { type: String, required: true },
+  body: { type: String, required: true },
+  image: { type: String, default: null },
+  imageAlt: { type: String, default: 'Image' },
+});
+</script>
+
+<style lang="scss">
+.card-content {
+  display: flex;
+  flex-direction: column;
+  gap: 0.3rem;
+
+  &__title {
+    @include headlineMedium;
+    color: inherit;
+  }
+
+  &__body {
+    @include bodyMedium;
+    max-width: 68%;
+    color: inherit;
+  }
+
+  &__image {
+    flex-shrink: 0;
+    width: 240px;
+    height: 240px;
+    border-radius: 0.5rem;
+    overflow: hidden;
+    margin-bottom: 40px;
+    position: absolute;
+    bottom: -120px;
+    right: -60px;
+
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
+  }
+}
+</style>
---
+++ components/card/Wrapper.vue
@@ -0,0 +1,100 @@
+<template>
+  <div v-if="!goTo" :class="['card', `card--${variant}`]">
+    <CardContent
+      :title="title"
+      :body="body"
+      :image="image"
+      :imageAlt="imageAlt"
+    />
+    <slot></slot>
+  </div>
+  <NuxtLink
+    v-else
+    :to="goTo"
+    :class="['card', `card--${variant} card--clickable`]"
+  >
+    <CardContent
+      :title="title"
+      :body="body"
+      :image="image"
+      :imageAlt="imageAlt"
+    />
+    <slot></slot>
+  </NuxtLink>
+</template>
+
+<script setup>
+defineProps({
+  title: { type: String, required: true },
+  body: { type: String, required: true },
+  variant: {
+    type: String,
+    default: 'default',
+    validator: value =>
+      ['default', 'backend', 'frontend', 'frameworks', 'designtools'].includes(
+        value
+      ),
+  },
+  goTo: {
+    type: String,
+    default: null,
+  },
+  image: {
+    type: String,
+    default: null,
+  },
+  imageAlt: {
+    type: String,
+    default: 'Image',
+  },
+});
+</script>
+
+<style lang="scss">
+.card {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-start;
+  padding: 1.2rem;
+  border-radius: 0.75rem;
+  gap: 1rem;
+  overflow: hidden;
+  position: relative;
+  min-height: 10rem;
+  text-decoration: none !important;
+  transition: all 0.2s ease-out;
+
+  &--clickable {
+    &:hover {
+      transform: translateY(-0.3rem);
+      box-shadow: 0 0.5rem 1rem rgba(0, 0, 0, 0.1);
+    }
+    &:active {
+      transform: scale(0.98);
+      box-shadow: 0 0.25rem 0.5rem rgba(0, 0, 0, 0.1);
+    }
+  }
+  /* Variants */
+  &--default {
+    background-color: $secondary;
+    color: $on-secondary !important;
+  }
+  &--backend {
+    background-color: $backend;
+    color: $on-backend !important;
+  }
+  &--frontend {
+    background-color: $frontend;
+    color: $on-frontend !important;
+  }
+  &--frameworks {
+    background-color: $frameworks;
+    color: $on-frameworks !important;
+  }
+  &--designtools {
+    background-color: $design-tools;
+    color: $on-design-tools !important;
+  }
+}
+</style>
