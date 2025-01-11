834688a9541b8a5e5cd2c50afa67f7a12bface37
Jonas Alvarson <jonas.alvarsson@gmail.com>
2025-01-11 14:27:00 +0100

    Add component base for Nabvar

---
+++ components/NavBar.vue
@@ -0,0 +1,73 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <header class="header">
+    <NuxtLink to="/" class="header__logo" title="Back to home">TechQuiz</NuxtLink>
+    <nav class="header__menu">
+      <ul class="header__menu-links">
+        <li class="header__menu-item">
+          <NuxtLink to="/quiz">Leaderboard</NuxtLink>
+        </li>
+        <li class="header__menu-item">
+          <NuxtLink to="/about">Quizes</NuxtLink>
+        </li>
+      </ul>
+      <BaseButton type="button" size="medium" label="Take the Quiz now!" />
+    </nav>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 1rem;
+  background-color: $secondary;
+  height: 58px;
+  overflow: hidden;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  z-index: 1000;
+
+  &__logo {
+    @include titleLarge;
+    color: $on-secondary;
+    text-decoration: none;
+  }
+
+  &__menu {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: row;
+    gap: 24px;
+
+    &-links {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      gap: 1rem;
+
+      &-item {
+        margin: 0 1rem;
+
+        &-link {
+          @include bodySmall;
+          color: $on-secondary;
+          text-decoration: none;
+          transition: color 0.3s;
+
+          &:hover {
+            color: $primary;
+          }
+        }
+      }
+    }
+  }
+}
+</style>
