23c4e3a9cf1c93ee794e13eb7ef3ef370124911b
Jonas Alvarson <jonas.alvarsson@gmail.com>
2025-01-14 09:30:27 +0100

    Add styling for quiz result component

--- components/quiz/Result.vue
+++ components/quiz/Result.vue
@@ -57,3 +57,42 @@ onMounted(async () => {
     </div>
   </div>
 </template>
+
+<style scoped lang="scss">
+.quiz-result {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 2rem;
+
+  &__header {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    max-width: 680px;
+    gap: 0.5rem;
+
+    .header-title {
+      @include displayLarge;
+    }
+
+    .header-subtitle {
+      @include titleLarge;
+    }
+
+    .header-compare {
+      @include bodyLarge;
+      margin-top: 1rem;
+      text-align: center; 
+    }
+  }
+
+  &__actions {
+    display: flex;
+    gap: 0.5rem;
+  }
+}
+</style>
