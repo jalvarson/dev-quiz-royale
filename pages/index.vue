6cdf47047e9b919b06cc612f78fa18c9210ae787
Jonas Alvarson <jonas.alvarsson@gmail.com>
2025-01-16 20:58:30 +0100

    Add parallax scrolling effect with tech logos

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,27 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted, onUnmounted } from 'vue';
+
+const handleParallax = () => {
+  const images = document.querySelectorAll('.parallax-image');
+  const scrollPosition = window.scrollY;
+
+  images.forEach(image => {
+    const isMoveUp = image.classList.contains('move-up');
+    const direction = isMoveUp ? -1 : 1;
+    const speed = parseFloat(image.getAttribute('data-speed') || '0.2');
+    const offset = scrollPosition * speed * direction;
+    (image as HTMLElement).style.transform = `translateY(${offset}px)`;
+  });
+};
+
+onMounted(() => {
+  window.addEventListener('scroll', handleParallax);
+});
+
+onUnmounted(() => {
+  window.removeEventListener('scroll', handleParallax);
+});
+</script>
 
 <template>
   <section class="main-header">
@@ -21,6 +44,44 @@
         label="Discover Quizzes"
         :onPress="() => navigateTo('/quiz')"
       />
+      <div class="parallax-container">
+        <NuxtImg
+          class="parallax-image move-up image-1"
+          data-speed="0.1"
+          src="/images/firebase-blurred.png"
+          alt="Firebase"
+        />
+        <NuxtImg
+          class="parallax-image move-up image-2"
+          data-speed="0.4"
+          src="/images/flutter-blurred.png"
+          alt="Flutter"
+        />
+        <NuxtImg
+          class="parallax-image move-down image-3"
+          data-speed="0.7"
+          src="/images/python.png"
+          alt="Python"
+        />
+        <NuxtImg
+          class="parallax-image move-up image-4"
+          data-speed="0.8"
+          src="/images/go.png"
+          alt="GoLang"
+        />
+        <NuxtImg
+          class="parallax-image move-down image-5"
+          data-speed="0.35"
+          src="/images/c.png"
+          alt="C++"
+        />
+        <NuxtImg
+          class="parallax-image move-down image-6"
+          data-speed="0.05"
+          src="/images/vue.png"
+          alt="VueJS"
+        />
+      </div>
     </div>
   </section>
 
@@ -71,20 +132,99 @@
   </LayoutsSection>
 
   <LayoutsSection :columns="1">
-    <Leaderboard id="leaderboard"/>
+    <Leaderboard id="leaderboard" />
   </LayoutsSection>
 </template>
 
 <style scoped lang="scss">
+.parallax-container {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  z-index: -1;
+  overflow: hidden;
+  pointer-events: none;
+}
+
+.parallax-image {
+  position: absolute;
+  will-change: transform;
+  transition: transform 0.4s ease-out;
+}
+
+.image-1 {
+  top: 5%;
+  left: -10%;
+  width: 300px;
+  @include respond-to('tablet') {
+    width: 200px;
+  }
+}
+
+.image-2 {
+  bottom: -10%;
+  right: 15%;
+  width: 400px;
+}
+
+.image-3 {
+  bottom: 20%;
+  left: -4%;
+  width: 240px;
+  @include respond-to('tablet') {
+    width: 200px;
+    bottom: 10%;
+    left: -16%;
+  }
+}
+
+.image-4 {
+  bottom: 10%;
+  right: 0%;
+  margin-right: -10px;
+  width: 300px;
+  @include respond-to('tablet') {
+    width: 200px;
+    margin-right: -50px;
+    bottom: 0%;
+  }
+}
+
+.image-5 {
+  top: 10%;
+  right: 20%;
+  width: 200px;
+  @include respond-to('tablet') {
+    width: 160px;
+    top: 6%;
+    right: 6%;
+  }
+}
+
+.image-6 {
+  top: 20%;
+  left: 10%;
+  width: 120px;
+  @include respond-to('tablet') {
+    width: 80px;
+    top: 20%;
+    left: 5%;
+  }
+}
+
 .main-header {
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+
   height: 100svh;
   padding: 0 1.4rem;
 
   &__container {
+    position: relative;
     display: flex;
     flex-direction: column;
     justify-content: center;
